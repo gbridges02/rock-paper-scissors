5c57a86ad9a67c2494a104a1b7742f537fb65faa
gbridges02 <gsbdd02@gmail.com>
2024-09-24 13:04:39 -0400

    add getHumanChoice function

--- script.js
+++ script.js
@@ -11,4 +11,22 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    humanChoice = prompt("Time to play Rock Paper Scissors! Make your decision now: ");
+    humanChoice = humanChoice.toLowerCase();
+    if(humanChoice === "rock"){
+        return "Rock";
+    }
+    else if(humanChoice === "paper"){
+        return "Paper";
+    }
+    else if(humanChoice === "scissors"){
+        return "Scissors";
+    }
+    else {
+        return "That's not a valid answer!";
+    }
+}
+
+console.log(getHumanChoice());
 console.log(getComputerChoice());
